98421850325e8e7cf215dfea43fe7263fafb3735
MasterShan <Henrik@vevatne.no>
2019-01-16 09:01:55 +0100

    Update Transmitter_Code.py

--- Transmitter_Code.py
+++ Transmitter_Code.py
@@ -3,39 +3,43 @@ from microbit import *
 #imports the microbit radio library
 import radio
 
-#turns the microbit radio on
 radio.on()
-#sets the channel the radio will transmit at
 radio.config(channel=10)
 
 
 while True:
-    #creates a variable so i dont have to write som much
-    gesture = accelerometer.current_gesture()
-    #if the microbit is tilting upwards, send "down"
-    if gesture == "face up":
+    x = accelerometer.get_x()
+    y = accelerometer.get_y()
+    
+    
+    if y>180
         radio.send("down")
-        
-    #if the microbit is tilting downwards, send "up"
-    elif gesture == "face down":
+       
+    elif y<-180
         radio.send("up")
         
-    #if the microbit is tilting left, send "left"    
-    elif gesture == "left":
+    elif x>180
         radio.send("left")
-        
-    #if the microbit is tilting right, send "right"    
-    elif gesture == "right":
+       
+    elif x<-180
         radio.send("right")
+    
+    if x>180 and y>180
+        radio.send("br")
+    
+    if x<-180 and y<-180
+        radio.send("tl")
+    
+    if x<-180 and y>180
+        radio.send("bl")
+    
+    if x>180 and y<-180
+        radio.send("tr")    
         
-    #if button a is pressed send "plus"  
     if button_a.is_pressed():
         radio.send("plus")
-        #makes the microbit stop for 0.5 seconds
         sleep(50)
         
-    #if button b is pressed send "minus"
     if button_b.is_pressed():
         radio.send("minus")
-        #makes the microbit stop for 0.5 seconds
         sleep(50)
