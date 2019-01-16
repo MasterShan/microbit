970791c99a8a08c6ec964378cc54459a700f9ee4
MasterShan <Henrik@vevatne.no>
2019-01-16 09:31:28 +0100

    Update Transmitter_Code.py

--- Transmitter_Code.py
+++ Transmitter_Code.py
@@ -4,14 +4,16 @@ from microbit import *
 import radio
 
 radio.on()
+#configures the channel on which channel to transmit information
 radio.config(channel=10)
 
 
 while True:
+    #get´s x and y values from the micobit
     x = accelerometer.get_x()
     y = accelerometer.get_y()
     
-    
+    #if the y or x value are larger or smaller than 180, send message
     if y>180
         radio.send("down")
        
@@ -23,7 +25,7 @@ while True:
        
     elif x<-180
         radio.send("right")
-    
+    #if both values correspond, send messsage
     if x>180 and y>180
         radio.send("br")
     
@@ -35,7 +37,7 @@ while True:
     
     if x>180 and y<-180
         radio.send("tr")    
-        
+      #if a button is pressed send message and delay  
     if button_a.is_pressed():
         radio.send("plus")
         sleep(50)
